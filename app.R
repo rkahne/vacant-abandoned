9549e4cee58a9b83211d0f727420867f0a426731
Robert Kahne <rkahne@edumedics.com>
2016-01-29 17:09:25 -0500

    Built a Shiny App for the Vacant and Abandoned Properties Project

---
+++ app.R
@@ -0,0 +1,56 @@
+.libPaths('C:/R-lib')
+library(shiny)
+library(xtable)
+library(scales)
+options(stringsAsFactors = F)
+
+
+data<-read.csv('./TaxDue_TextFormat.csv')
+LouisvilleZips <- c(40212,40211,40216,40258,40272,40203,40210,40208,40215,40214,40118,40209,40202,40206,40204,40217,
+                               40213,40219,40229,40205,40218,40228,40291,40220,40299,40023,40245,40059,40241,40223,40243,
+                               40242,40025,40222,40207,40177,40231,40041,40280)
+data<-subset(data, TAXYEAR>2006 & STATE=='KY')
+data$ZIP<-substr(data$ZIP,1,5)
+data<-subset(data,ZIP %in% LouisvilleZips)
+leinFlag<-subset(data, LIENFLAG != '')
+unpaid<-subset(data, FULLPAID != 'Y')
+
+
+ui <- fluidPage(
+  
+  #Application Title
+  headerPanel("Vacant And Abandoned Properties"),
+  #Sidebar
+  sidebarPanel(
+    checkboxGroupInput('zip', 'Which Zip Codes?', sort(LouisvilleZips)),
+    checkboxGroupInput('year', 'For Which Years?', sort(unique(data$TAXYEAR)))
+  ),
+  mainPanel(
+    textOutput('totalDue'),
+    tableOutput('dueToWho')
+  )
+)
+
+server <- function(input, output){
+  output$totalDue<-renderText({
+    outData<-subset(data, ZIP %in% input$zip & TAXYEAR %in% input$year)
+    paste('Total Due in Area Selected: ',dollar_format()(sum(outData$BILLTOT)),sep='')
+    
+  })
+  
+  output$dueToWho<-renderTable({
+    outData<-subset(data, ZIP %in% input$zip & TAXYEAR %in% input$year)
+    newData<-data.frame(unique(outData$PDBYSOLDTO))
+    colnames(newData)<-'PaidBySoldTo'
+    newData$Total<-sapply(newData$PaidBySoldTo,function(i){
+      sum(outData$BILLTOT[which(outData$PDBYSOLDTO == i)])
+    })
+    newData$NumberOfProperties<-sapply(newData$PaidBySoldTo,function(i){
+      length(which(outData$PDBYSOLDTO == i))
+    })
+    newData$PaidBySoldTo[which(newData$PaidBySoldTo=='')]<-'Metro Louisville'
+    return(newData)
+  })
+}
+
+shinyApp(ui = ui, server = server)
